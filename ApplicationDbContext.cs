810aad9f64978547097c3060201ccb509a8b71ca
Erick Jovan <erickjovan106@gmail.com>
2023-10-19 19:13:28 -0600

    add entity and migration for books

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 using Microsoft.EntityFrameworkCore;
+using WebApiAutores.Entidades;
 
 namespace WebApiAutores
 {
     public class ApplicationDbContext : DbContext
     {
-        public DbSet <Autor> Autores { get; set; }
+        public DbSet<Autor> Autores { get; set; }
+        public DbSet<Libro> Libros { get; set; }
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
